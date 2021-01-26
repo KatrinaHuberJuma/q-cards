495c4d9606b8f9e7166c0bffebd52a56a6b5edcf
Katrina Huber <katrina.huber.juma@gmail.com>
2021-01-26 00:20:18 +0000

    get fetch response working and use React.useEffect to render on page load

--- static/js/queue.jsx
+++ static/js/queue.jsx
@@ -2,33 +2,31 @@
 // TODO: Why "unreachable code after return statement"
 
 function Queue(){
-    fetch("/cards.json")
-    .then((response) => {
-        // console.log(response);
-        let res = response;
-        let resJSON = response.json();
-        console.log(res);
-        console.log(resJSON);
-        return res
-    })
 
-    .catch((error)=> {
-        console.log(error)
-    });
+    const [cardsData, setCardsData] = React.useState([]);
+    
+    React.useEffect(() => {
+        fetch("/cards.json")
+        .then(response => response.json())
+        .then(data => setCardsData(data))
+    }, [])
+
+    const cards = [];
+
+    for(const card of cardsData){
+        cards.push(
+            <Card 
+                studentName={card.studentName}
+                imgUrl={card.imgUrl}
+                question={card.question}
+            />
+        )
+    }
 
     return (
       <React.Fragment>
         <h1>this exists</h1>
-        <Card
-            studentName="Athelia" 
-            imgUrl="https://fellowship.hackbrightacademy.com/media/CACHE/images/staff/athelia/d3a4891536121a55505ac626945056d9.jpeg"
-            question="Jinja template doesn't show my navbar"
-        />
-        <Card
-            question="Can't get flask route to render template. Getting Method Not Allowed"
-            studentName="Kat" 
-            imgUrl="https://fellowship.hackbrightacademy.com/media/CACHE/images/students/selfie/d5cb095e71fd4f86f87fed80c05d93d2.jpg"
-        />
+        {cards}
       </React.Fragment>
     )
   }
