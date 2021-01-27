434883ade4a53fde444ff4df587e33b34c16da10
Athelia Crosmun <hi@athelia.codes>
2021-01-27 23:08:59 +0000

    Fix fetch POST to server from EnqueueForm

--- server.py
+++ server.py
@@ -20,6 +20,19 @@ def return_mock_info():
     return jsonify(queue_cards)
 
 
+@app.route('/enqueue-submit', methods=["POST"])
+def handle_enqueue_submit():
+    """Store form data"""
+
+    # import pdb; pdb.set_trace()
+
+    # title = request.form.get('title')
+    title = request.json['title']
+    print('\n'*5, '*'*20, title, '\n'*5)
+    # title is None
+    # print('\n'*5, '*'*20, request.form, '\n'*5)
+    return jsonify('YAS')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5050')
--- static/js/enqueueForm.jsx
+++ static/js/enqueueForm.jsx
@@ -1,19 +1,36 @@
 "use strict";
 
 function EnqueueForm(){
-    const [greatness, setGreatness] = React.useState("");
+    const [issueTitle, setIssueTitle] = React.useState("");
+
 
     const handleSubmit = (evt) => {
         evt.preventDefault();
-        alert(greatness)
+        alert(issueTitle)
+
+        const options = {
+            method: "POST", 
+            body: JSON.stringify({title: issueTitle}),
+            headers: { 
+                "Content-type": "application/json; charset=UTF-8"
+            } 
+        }
+
+        fetch("/enqueue-submit", options)
+        .then(response => response.json())
+        .then((result) => result)
+        .then(data => alert(data))
+        .catch((error) => {
+            console.error('Error:', error);
+        });
     }
 
     return (<form onSubmit={handleSubmit}>
         <input 
             type="text" 
-            placeholder="tell me about your greatness" 
-            value={greatness} 
-            onChange={e => setGreatness(e.target.value)}
+            placeholder="tell me about your issueTitle" 
+            value={issueTitle} 
+            onChange={e => setIssueTitle(e.target.value)}
         />
         <input type="submit" />
     </form>)
