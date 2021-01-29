9147f14897be800751de4bfe08b6dddeb2549983
Athelia Crosmun <hi@athelia.codes>
2021-01-29 17:31:24 +0000

    Add loggedIn state to <Main />; pass handleLogin via props to form
    Co-authored-by: Katrina Huber-Juma <katrina.huber@gmail.com>

--- static/js/loginForm.jsx
+++ static/js/loginForm.jsx
@@ -1,7 +1,47 @@
 "use strict";
 
-function LoginForm() {
+function LoginForm(props) {
+    
+    const [userName, setUserName] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        // console.log(`We call you ${userName}`);
+        // console.log(`Secret code ${password}`);
+        const options = {
+            method : 'POST',
+            body: JSON.stringify({
+                'userName': userName,
+                'password': password
+            }),
+            headers: { 
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        }
+
+        fetch('/login', options)
+        .then(response => response.json())
+        .then(data => {alert(data)})
+        .catch(err => {console.error(`ERROR ${err}`)})
+
+    }
+
     return (
-        <h1>Yo login time</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="userName">What do we call you?</label>
+            <input 
+                id="userName" 
+                onChange={e=>{setUserName(e.target.value)}} 
+            />
+            
+            <label htmlFor="password">Secret access code</label>
+            <input 
+                id="password" 
+                onChange={e=>{setPassword(e.target.value)}} 
+            />
+            <input type="submit" />
+            <button onClick={props.handleLogin}>I shall soon die</button>
+        </form>
     )
 }
--- static/js/main.jsx
+++ static/js/main.jsx
@@ -1,7 +1,20 @@
 "use strict";
 function Main() {
+    
+    const [loggedIn, setLoggedIn] = React.useState(false)
+
+    const handleLogin = () => {
+        setLoggedIn(!loggedIn);
+    }
+
+    if (loggedIn) {
+        return (
+            <h1>Bat cave access granted</h1>
+        )
+    }
+
     return ( <React.Fragment>
-        <LoginForm />
+        <LoginForm handleLogin={handleLogin} />
         <Enqueue />
         <Queue />
     </React.Fragment>)
