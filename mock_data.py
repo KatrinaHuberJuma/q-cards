94c95eef2b118986d6c40a8bafd492d14afe5378
Katrina Huber <katrina.huber.juma@gmail.com>
2021-02-03 22:33:50 +0000

    Update with more data
    Co-authored-by: Athelia Crosmun <hi@athelia.codes>

--- mock_data.py
+++ mock_data.py
@@ -2,28 +2,53 @@
 queue_cards = [
     {
         "studentName":"Athelia",
-        "title": "some title",
+        "title": "Athelia's title",
+        "isActive": True,
         "imgUrl":"https://fellowship.hackbrightacademy.com/media/CACHE/images/staff/athelia/d3a4891536121a55505ac626945056d9.jpeg",
         "question":"Jinja template doesn't show my navbar",
     },
     {
         "question":"response in callback of .then showing up in console as Promise: {'pending'}",
-        "title": "some title",
+        "title": "Kat's title",
         "studentName":"Kat",
+        "isActive": True,
         "imgUrl":"https://fellowship.hackbrightacademy.com/media/CACHE/images/students/selfie/d5cb095e71fd4f86f87fed80c05d93d2.jpg",
     },
     {
         "question":"fetch with POST is not being received by the server. method is correct in both the JS and server.py",
-        "title": "some title",
+        "title": "Thu's title (not active)",
         "studentName":"Thu",
+        "isActive": False,
         "imgUrl":"https://fellowship.hackbrightacademy.com/media/CACHE/images/students/3101D95D-93EA-482C-A867-B1170FCAAFA9/49dee8b24eb2d07b28ca897c4f911613.jpeg",
     },
     {
         "question": "Ready for a code review",
-        "title": "some title",
+        "title": "Andrew's title",
         "studentName": "Andrew",
+        "isActive": True,
         "imgUrl": "https://fellowship.hackbrightacademy.com/media/CACHE/images/staff/andrew/32238b47f4ad742072e16f0ba1cb1794.jpeg"
-    }
+    },
+    {
+        "question":"fetch with POST is not being received by the server. method is correct in both the JS and server.py",
+        "title": "Lucia's title (not active)",
+        "studentName":"Lucia",
+        "isActive": False,
+        "imgUrl":"lol later",
+    },
+    {
+        "question":"fetch with POST is not being received by the server. method is correct in both the JS and server.py",
+        "title": "Anjelica's title (not active)",
+        "studentName":"Anjelica",
+        "isActive": False,
+        "imgUrl":"lol later",
+    },
+    {
+        "question":"fetch with POST is not being received by the server. method is correct in both the JS and server.py",
+        "title": "Maily's title (not active)",
+        "studentName":"Maily",
+        "isActive": False,
+        "imgUrl":"lol later",
+    },
 ]
 
 users = [
