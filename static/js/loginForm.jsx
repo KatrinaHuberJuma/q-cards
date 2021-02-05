fcbe058f82d851b2b2025084b9557bfb415fd87d
Katrina Huber <katrina.huber.juma@gmail.com>
2021-02-05 17:09:56 +0000

    fix indentation from 4 spaces to 2 (thank you space fairy athelia)
    Co-authored-by: Athelia Crosmun <hi@athelia.codes>

--- static/js/loginForm.jsx
+++ static/js/loginForm.jsx
@@ -2,59 +2,57 @@
 
 function LoginForm(props) {
     
-    const [userName, setUserName] = React.useState('');
-    const [password, setPassword] = React.useState('');
+const [userName, setUserName] = React.useState('');
+const [password, setPassword] = React.useState('');
 
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
-        // console.log(`We call you ${userName}`);
-        // console.log(`Secret code ${password}`);
-        const options = {
-            method : 'POST',
-            body: JSON.stringify({
-                'userName': userName,
-                'password': password
-            }),
-            headers: { 
-                'Content-type': 'application/json; charset=UTF-8'
-            }
-        }
+const handleSubmit = (evt) => {
+  evt.preventDefault();
+  // console.log(`We call you ${userName}`);
+  // console.log(`Secret code ${password}`);
+  const options = {
+    method : 'POST',
+    body: JSON.stringify({
+      'userName': userName,
+      'password': password
+    }),
+    headers: {'Content-type': 'application/json; charset=UTF-8'}
+  }
 
-        fetch('/login', options)
-        .then(response => {
-            if (response.status === 200){
-                props.handleLogin()
-                return response.json();
-            } else {
-                console.error(`ERROR ${response.statusText}`)
-            }
-        })
-        .then(data => {
-            console.log('about to deal with some cool data', data)
-            if (data.isStaff){
-                console.log('data.isStaff seems true....')
-                props.declareStaff();
-            }
-        })
-        .catch(err => {console.error(`ERROR ${err}`)})
-
+  fetch('/login', options)
+  .then(response => {
+    if (response.status === 200){
+      props.handleLogin()
+      return response.json();
+    } else {
+      console.error(`ERROR ${response.statusText}`)
     }
+  })
+  .then(data => {
+    console.log('about to deal with some cool data', data)
+    if (data.isStaff){
+      console.log('data.isStaff seems true....')
+      props.declareStaff();
+    }
+  })
+  .catch(err => {console.error(`ERROR ${err}`)})
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="userName">What do we call you?</label>
-            <input 
-                id="userName" 
-                onChange={e=>{setUserName(e.target.value)}} 
-            />
-            
-            <label htmlFor="password">Secret access code</label>
-            <input 
-                id="password" 
-                onChange={e=>{setPassword(e.target.value)}} 
-            />
-            <input type="submit" />
-            {/* <button onClick={props.handleLogin}>I shall soon die</button> */}
-        </form>
-    )
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="userName">What do we call you?</label>
+      <input 
+        id="userName" 
+        onChange={e=>{setUserName(e.target.value)}} 
+      />
+      
+      <label htmlFor="password">Secret access code</label>
+      <input 
+        id="password" 
+        onChange={e=>{setPassword(e.target.value)}} 
+      />
+      <input type="submit" />
+      {/* <button onClick={props.handleLogin}>I shall soon die</button> */}
+    </form>
+  )
 }
