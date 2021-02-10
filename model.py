9b2e323cda2feb4de57e23c2ea5ad55ff860e6f8
Katrina Huber <katrina.huber.juma@gmail.com>
2021-02-10 16:55:34 +0000

    complete user, staff, student model
    Co-authored-by: Athelia Crosmun <hi@athelia.codes>

--- model.py
+++ model.py
@@ -9,37 +9,90 @@ class User(db.Model):
 
     __tablename__ = 'users'
 
+    # TODO: Update nullables below once testing complete
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
-    is_staff = db.Column(db.Boolean)
+    image_url = db.Column(db.String)
+
+    # staff = relationship to one row in staff table, if user is staff
+    # student = relationship to one row in students table, if user is student
 
     def __repr__(self):
-        return f'<User user_id={self.user_id} username={self.username} first_name={self.first_name} last_name={self.last_name} email={self.email} password={self.password}>'
+        return f'<User user_id={self.user_id} first_name={self.first_name} last_name={self.last_name}>'
+
+class Staff(db.Model):
+
+    __tablename__ = 'staff'
+
+    staff_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    # Question: can we just use the user_id as the primary key and also the foreign key
+    job_title = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, 
+                        db.ForeignKey('users.user_id'),
+                        nullable=False)
+    
+    user = db.relationship('User', 
+                        backref=db.backref('staff', uselist=False))
+
+    def __repr__(self):
+
+        return f'<Staff staff_id={self.staff_id}, job_title={self.job_title}>'
 
 
+class Student(db.Model):
 
-class Plea(db.Model): # TODO is this a terrible name?
-    """ plea_id, user_id, title, question, details"""
+    __tablename__ = 'students'
 
-    __tablename__ = 'pleas'
-
-    plea_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    student_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    computer = db.Column(db.String, nullable=False)
+    github = db.Column(db.String, nullable=False)
     user_id = db.Column(db.Integer,
                         db.ForeignKey('users.user_id'),
-                        nullable=False) # TODO probably should be one to many, because pairs
-    title = db.Column(db.String, nullable=False)
-    question = db.Column(db.String, nullable=False)
-    details = db.Column(db.String)
+                        nullable=False)
+    
+    user = db.relationship('User',
+                        backref=db.backref('student', uselist=False))
+    
+    def __repr__(self):
 
-    student = db.relationship('User', backref='pleas')
+        return f'<Student student_id{self.student_id}, github={self.github}, computer={self.computer}>'
+
+
+
+
+
+
+
+# class Plea(db.Model): # TODO is this a terrible name?
+#     """ 
+    
+#     cols: plea_id, user_id, title, question, details"""
+
+#     __tablename__ = 'pleas'
+
+#     plea_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     user_id = db.Column(db.Integer,
+#                         db.ForeignKey('users.user_id'),
+#                         nullable=False) # TODO probably should be one to many, because pairs
+#     title = db.Column(db.String, nullable=False)
+#     question = db.Column(db.String, nullable=False)
+#     details = db.Column(db.String)
+
+#     student = db.relationship('User', backref='pleas')
+
+
+# class UserPlea(db.Model):
+
+#     user_plea_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    
     
 
-    def __repr__(self):
+    # def __repr__(self):
         
-        return f"<Plea plea_id = {self.plea_id}, title = {self.title}>"
+    #     return f"<Plea plea_id = {self.plea_id}, title = {self.title}>"
 
 
 
@@ -48,7 +101,8 @@ class Plea(db.Model): # TODO is this a terrible name?
 
 # =================
 
-def connect_to_db(flask_app, db_uri='postgresql:///qtestdb', echo=True):
+def connect_to_db(flask_app, db_uri='postgresql:///qtestdb', echo=True): 
+    # FIXME maybe better db name?
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     flask_app.config['SQLALCHEMY_ECHO'] = echo
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- seed_data.py
+++ seed_data.py
@@ -1,4 +1,4 @@
-from model import User, Plea, connect_to_db, db
+from model import *
 import os 
 import server
 
@@ -12,8 +12,7 @@ db.create_all()
 ac = User(  first_name = "Athelia", 
             last_name = "Crosmun", 
             email = "email@email.email", 
-            password = "password",
-            is_staff = True
+            password = "password"
         )
 
 khj = User( first_name = "Katrina", 
@@ -22,12 +21,21 @@ khj = User( first_name = "Katrina",
             password = "password"
         )
 
-p1 = Plea(  title = "Weekend is over", 
-            question = "Where did all the time go?", 
-            details = "Like I'm happy to get back to work and see everyone, but I thought I'd accomplish more, you know?",
-            student = khj
-            )
+ac_staff = Staff(
+    job_title = 'Goddess of Greatness',
+    user = ac
+)
+
+khj_student = Student(  computer='mac', 
+                        github='katrinahuberjuma', 
+                        user=khj)
+
+# p1 = Plea(  title = "Weekend is over", 
+#             question = "Where did all the time go?", 
+#             details = "Like I'm happy to get back to work and see everyone, but I thought I'd accomplish more, you know?",
+#             student = khj
+#             )
 
 db.session.add(ac)
-db.session.add(p1)
+db.session.add(ac_staff)
 db.session.commit()
