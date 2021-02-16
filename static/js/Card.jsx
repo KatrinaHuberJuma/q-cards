3fe68de5606f2e48c1301965182a64b1f3df19f0
Katrina Huber-Juma <Katrina.huber@gmail.com>
2021-02-16 07:33:06 -0800

    props destructured like a boss, also ternary (chirp chirp, huzzah)
    Co-authored-by: Athelia Crosmun <hi@athelia.codes>

--- static/js/Card.jsx
+++ static/js/Card.jsx
@@ -1,17 +1,13 @@
 "use strict";
 
-function Card({studentName, showDequeue, imgUrl, question} ){
-  // that's props destructuring 
-  // TODO FIXME remove all the props.
-
-  // const {studentName, showDequeue, imgUrl, question} = props;
+function Card({studentName, showDequeue, imgUrl, question, handleDequeue} ){
 
   const cardHandleDequeue = () => {
-    props.handleDequeue(props.studentName);
+    handleDequeue(studentName);
     const options = { 
       method: 'POST',
       body: JSON.stringify({'card': {
-        studentName: props.studentName // TODO: should be question id
+        studentName: studentName // TODO: should be question id
       }}),
       headers: {'Content-type': 'application/json; charset=UTF-8'}
     }
@@ -20,20 +16,11 @@ function Card({studentName, showDequeue, imgUrl, question} ){
     .then(response => response.json())
     .then(data => console.log(data))
   }
-// Question: is this too repetitive, is better possible?
-  if (props.showDequeue){
-    return ( <div className="card">
-      <h1>{props.studentName}</h1>
-      <img src={props.imgUrl}/>
-      <p>{props.question}</p>
-      {/* TODO: handle whether to show dequeue button in ternary */}
-      <button id={props.studentName} onClick={cardHandleDequeue}>Dequeue</button>
-    </div>)
-  }else{
-    return ( <div className="card">
-      <h1>{props.studentName}</h1>
-      <img src={props.imgUrl}/>
-      <p>{props.question}</p>
-    </div>)
-  }
+
+  return ( <div className="card">
+    <h1>{studentName}</h1>
+    <img src={imgUrl}/>
+    <p>{question}</p>
+    { showDequeue ? <button id={studentName} onClick={cardHandleDequeue}>Dequeue</button> : null }
+  </div>)
 }
