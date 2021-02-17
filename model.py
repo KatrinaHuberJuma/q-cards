19003012b536e797dff82bbff3ed05bd911a6c09
Katrina Huber <katrina.huber.juma@gmail.com>
2021-02-17 15:59:31 +0000

    Data remodeling - eliminate students and staff; create Question table
    Co-authored-by: Athelia Crosmun <hi@athelia.codes>

--- model.py
+++ model.py
@@ -16,83 +16,42 @@ class User(db.Model):
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
     image_url = db.Column(db.String)
-
-    # staff = relationship to one row in staff table, if user is staff
-    # student = relationship to one row in students table, if user is student
-
-    def __repr__(self):
-        return f'<User user_id={self.user_id} first_name={self.first_name} last_name={self.last_name}>'
-
-class Staff(db.Model):
-
-    __tablename__ = 'staff'
-
-    staff_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    # Question: can we just use the user_id as the primary key and also the foreign key
+    computer = db.Column(db.String)
+    github = db.Column(db.String) #TODO: put back non-nullability
     job_title = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.Integer, 
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} job_title={self.job_title} f+l name={self.first_name} {self.last_name}>'
+
+
+class Question(db.Model): # TODO is this a terrible name?
+    """ 
+    
+    cols: plea_id, user_id, title, question, details"""
+
+    __tablename__ = 'questions'
+
+    question_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    author_id = db.Column(db.Integer,
                         db.ForeignKey('users.user_id'),
                         nullable=False)
-    
-    user = db.relationship('User', 
-                        backref=db.backref('staff', uselist=False))
+    pair_id = db.Column(db.Integer,
+                        db.ForeignKey('users.user_id'))
+    # TODO: lab_id
+    title = db.Column(db.String, nullable=False)
+    desired_outcome = db.Column(db.String)
+    description = db.Column(db.String)
+    background = db.Column(db.String)
+    furtherInfo = db.Column(db.String)
+    efforts = db.Column(db.String)
+
+    author = db.relationship('User', backref='questions', foreign_keys=[author_id])
+    pair = db.relationship('User', backref='pair_questions', foreign_keys=[pair_id])
 
     def __repr__(self):
 
-        return f'<Staff staff_id={self.staff_id}, job_title={self.job_title}>'
+        return f'<Question question_id={self.question_id}, title={self.title}, author_id={self.author_id}>'
 
-
-class Student(db.Model):
-
-    __tablename__ = 'students'
-
-    student_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    computer = db.Column(db.String, nullable=False)
-    github = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.Integer,
-                        db.ForeignKey('users.user_id'),
-                        nullable=False)
-    
-    user = db.relationship('User',
-                        backref=db.backref('student', uselist=False))
-    
-    def __repr__(self):
-
-        return f'<Student student_id{self.student_id}, github={self.github}, computer={self.computer}>'
-
-
-
-
-
-
-
-# class Plea(db.Model): # TODO is this a terrible name?
-#     """ 
-    
-#     cols: plea_id, user_id, title, question, details"""
-
-#     __tablename__ = 'pleas'
-
-#     plea_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-#     user_id = db.Column(db.Integer,
-#                         db.ForeignKey('users.user_id'),
-#                         nullable=False) # TODO probably should be one to many, because pairs
-#     title = db.Column(db.String, nullable=False)
-#     question = db.Column(db.String, nullable=False)
-#     details = db.Column(db.String)
-
-#     student = db.relationship('User', backref='pleas')
-
-
-# class UserPlea(db.Model):
-
-#     user_plea_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    
-    
-
-    # def __repr__(self):
-        
-    #     return f"<Plea plea_id = {self.plea_id}, title = {self.title}>"
 
 
 
--- seed_data.py
+++ seed_data.py
@@ -12,32 +12,37 @@ db.create_all()
 ac = User(  first_name = "Athelia", 
             last_name = "Crosmun", 
             email = "email@email.email", 
-            password = "password"
+            password = "password",
+            computer='mac',
+            github='athelia',
+            job_title='student'
         )
 
 khj = User( first_name = "Katrina", 
             last_name = "Huber-Juma", 
             email = "email2@email.email", 
-            password = "password"
+            password = "password",
+            job_title='student'
         )
 
-ac_staff = Staff(
-    job_title = 'Goddess of Greatness',
-    user = ac
-)
 
-khj_student = Student(  computer='mac', 
-                        github='katrinahuberjuma', 
-                        user=khj)
 
-# p1 = Plea(  title = "Weekend is over", 
-#             question = "Where did all the time go?", 
-#             details = "Like I'm happy to get back to work and see everyone, but I thought I'd accomplish more, you know?",
-#             student = khj
-#             )
+q1 = Question(  title = "Weekend is over", 
+            description = "Where did all the time go?", 
+            desired_outcome = "Like I'm happy to get back to work and see everyone, but I thought I'd accomplish more, you know?",
+            author = khj
+            )
+
+q2 = Question(  title = "I was following a fox", 
+            description = "all swallowed in their coats", 
+            desired_outcome = "with scarves of red tied round their throats",
+            author = ac,
+            pair=khj
+            )
+
 
 # db.session.add(ac)
-db.session.add(ac_staff)
+db.session.add(q1)
 # db.session.add(khj)
-db.session.add(khj_student)
+db.session.add(q2)
 db.session.commit()
