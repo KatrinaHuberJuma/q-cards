7e339056f7619e1c642aec48e53074011f26abb7
Katrina Huber-Juma <Katrina.huber@gmail.com>
2021-02-19 11:09:48 -0800

    create fetchCards function inside Main component. Unable to revive dequeue finctionality before Athelia had to cinderella-out
    oh no, this is what I get for trying to perform unsupervised spelling
    
    Co-authored-by: Athelia Crosmun <hi@athelia.codes>

--- static/js/main.jsx
+++ static/js/main.jsx
@@ -6,6 +6,7 @@ function Main() {
   const [cardsData, setCardsData] = React.useState([]);
 
   const handleDequeue = (studentName) => {
+    // TODO: UP NEXT we no longer need to sort here, dequeue button currently broken
     console.log(`welcome to the archive, ${studentName}`);
     setCardsData(cardsData.map(card => {
       card.isActive = card.studentName === studentName ? false : card.isActive;
@@ -23,29 +24,19 @@ function Main() {
     setIsStaff(true);
   }
 
-  React.useEffect(() => {
-    // Question: move down to end of this file or to another file?
-    // TODO: move to own function (e.g. updateCards) and call from within here. then can call updateCards whenever the data should change
+  function fetchCards() {
+    // TODO: UP NEXT move this out of component?
     fetch("/cards.json")
     .then(response => response.json())
     .then(data => setCardsData(data))
-  }, [])
+  }
+
+  React.useEffect(fetchCards, [])
   
+  // React.useEffect(()=>{setCardsData(fetchCards)}, [])
   // console.log(cardsData);
   const activeCardData = cardsData.active;
   const miniCardData = cardsData.not_active;
-  
-  // // TODO: better in a useEffect because no need to re-render every time component mounts, only whenever data is actually changed
-  // // or better: have back end do more data manipulation
-  // for (const datum of cardsData){
-  //   if (datum.isActive){
-  //     // console.log(`I'm an active datum ${datum.studentName}`)
-  //     activeCardData.push(datum);
-  //   } else {
-  //     // console.log(`I'm a passive aggressive datum ${datum.studentName}`)
-  //     miniCardData.push(datum);
-  //   }
-  // }
 
   if (loggedIn) {
     return (
