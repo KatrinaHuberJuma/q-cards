ef8aece0e8bdea006c28c9bfb07fa2b09d632345
Katrina Huber <katrina.huber.juma@gmail.com>
2021-02-19 17:29:46 +0000

    create help functions for querying db, and for sorting, dictifying questions
    Co-authored-by: Athelia Crosmun <hi@athelia.codes>

---
+++ helpers.py
@@ -0,0 +1,36 @@
+from model import *
+
+def order_questions():
+    """Queries db for all questions with users"""
+    qq = Question.query.order_by(Question.is_active)
+    questions_w_users = qq.options( db.joinedload('author'), 
+                                    db.joinedload('pair')).all()
+
+    return questions_w_users
+
+
+def sort_question_dict(questions):
+    """Returns a dictionary with active and inactive keys for question dicts"""
+
+    result = {
+            'active': [],
+            'not_active': []
+    }
+
+    for question in questions:
+        if question.is_active:
+            result['active'].append(question.to_dict())
+        else:
+            result['not_active'].append(question.to_dict())
+
+    return result
+
+
+
+if __name__ == '__main__':
+    from server import app
+
+    connect_to_db(app)
+
+    print(order_questions())
+    print(sort_question_dict(order_questions()))
